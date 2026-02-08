a92ed218e0151bd4d1062d0f583cb7c6d81f1421
Mosallam Abdulrahman <mosallamabdulrahman@gmail.com>
2026-02-08 03:03:25 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
     port: 3000,
     host: '0.0.0.0',
   },
-  plugins: [react()],
   base: '/Online-Coaching/',
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
